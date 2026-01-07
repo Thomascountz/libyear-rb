c5f771ed5c5e36ff575644d597e6385896decc27
Thomas Countz <thomascountz@gmail.com>
2026-01-07 23:48:31 +0100

    Simplify PlaintextReporter
    - Base Reporter accepts an io
    - Simplify table generation
    - Assert against full table strings

--- lib/libyear_rb/reporters/reporter.rb
+++ lib/libyear_rb/reporters/reporter.rb
@@ -2,6 +2,10 @@
 
 module LibyearRb
   class Reporter
+    def initialize(io: $stdout)
+      @io = io
+    end
+
     def generate(dependency_freshness)
       raise NotImplementedError, "Subclasses must implement the generate method"
     end
